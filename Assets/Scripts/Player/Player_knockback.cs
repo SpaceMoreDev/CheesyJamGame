50c803049cf4044a4d21a2b3909e486db08e57c3
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-05-16 01:02:29 +0300

    Added Enemy Screenshake profile

--- Assets/Scripts/Player/Player_knockback.cs
+++ Assets/Scripts/Player/Player_knockback.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.WSA;
+using Cinemachine;
 
 public class Player_knockback : MonoBehaviour
 {
@@ -11,17 +13,25 @@ public class Player_knockback : MonoBehaviour
     public Camera mycam;
     public float pushforce;
 
+    private CinemachineImpulseSource impulseSource;
+
+    [SerializeField] ScreenShakeProfile profile;
+
     // Start is called before the first frame update
     void Start()
     {
         mycam = Camera.main;
+        
     }
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.CompareTag("Enemy"))
         {
             GetComponent<Rigidbody>().AddForce(mycam.transform.forward * -pushforce, ForceMode.Impulse);
+            impulseSource = col.gameObject.GetComponent<CinemachineImpulseSource>();
+            CameraShakeManager.instance.ScreenShakeFromProfile(profile, impulseSource);
         }
+       
     }
     
 }
