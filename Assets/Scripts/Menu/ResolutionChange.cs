d97e07673dbdcc85fcf5e7b0775c7fb850c382ba
zeyadmedhat <zeyad.medhat2000@gmail.com>
2024-05-16 21:43:48 +0300

    Implemented ScreenMode

--- Assets/Scripts/Menu/ResolutionChange.cs
+++ Assets/Scripts/Menu/ResolutionChange.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class ResolutionChange : MonoBehaviour
 {
     private TMP_Dropdown resoldrop;
+    [SerializeField] private TMP_Dropdown screenmode;
+    bool fullscreenmode;
 
     // Start is called before the first frame update
     void Start()
@@ -16,35 +18,98 @@ public class ResolutionChange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(screenmode.value == 0)
+        {
+            fullscreenmode = true;
+        }
+        else if(screenmode.value == 1)
+        {
+            fullscreenmode = false;
+        }
+        Screen.SetResolution(PlayerPrefs.GetInt("width"), PlayerPrefs.GetInt("height"), fullscreenmode);
     }
 
     public void changeresolution()
     {
-        if(resoldrop.value == 0)
+        if(screenmode.value == 0)
         {
-            Screen.SetResolution(1366, 768,true);
+            if (resoldrop.value == 0)
+            {
+                Screen.SetResolution(1366, 768,fullscreenmode);
+                PlayerPrefs.SetInt("width", 1366);
+                PlayerPrefs.SetInt("height", 768);
+            }
+            else if (resoldrop.value == 1)
+            {
+                Screen.SetResolution(1600, 900,fullscreenmode);
+                PlayerPrefs.SetInt("width", 1600);
+                PlayerPrefs.SetInt("height", 900);
+            }
+            else if (resoldrop.value == 2)
+            {
+                Screen.SetResolution(1920, 1080,fullscreenmode);
+                PlayerPrefs.SetInt("width", 1920);
+                PlayerPrefs.SetInt("height", 1080);
+            }
+            else if (resoldrop.value == 3)
+            {
+                Screen.SetResolution(1920, 1200,fullscreenmode);
+                PlayerPrefs.SetInt("width", 1920);
+                PlayerPrefs.SetInt("height", 1200);
+            }
+            else if (resoldrop.value == 4)
+            {
+                Screen.SetResolution(2560, 1440,fullscreenmode);
+                PlayerPrefs.SetInt("width", 2560);
+                PlayerPrefs.SetInt("height", 1440);
+            }
+            else if (resoldrop.value == 5)
+            {
+                Screen.SetResolution(2560, 1600,fullscreenmode);
+                PlayerPrefs.SetInt("width", 2560);
+                PlayerPrefs.SetInt("height", 1600);
+            }
         }
-        else if (resoldrop.value == 1)
+        else if (screenmode.value == 1)
         {
-            Screen.SetResolution(1600, 900, true);
+            if (resoldrop.value == 0)
+            {
+                Screen.SetResolution(1366, 768, fullscreenmode);
+                PlayerPrefs.SetInt("width", 1366);
+                PlayerPrefs.SetInt("height", 768);
+            }
+            else if (resoldrop.value == 1)
+            {
+                Screen.SetResolution(1600, 900, fullscreenmode);
+                PlayerPrefs.SetInt("width", 1600);
+                PlayerPrefs.SetInt("height", 900);
+            }
+            else if (resoldrop.value == 2)
+            {
+                Screen.SetResolution(1920, 1080, fullscreenmode);
+                PlayerPrefs.SetInt("width", 1920);
+                PlayerPrefs.SetInt("height", 1080);
+            }
+            else if (resoldrop.value == 3)
+            {
+                Screen.SetResolution(1920, 1200, fullscreenmode);
+                PlayerPrefs.SetInt("width", 1920);
+                PlayerPrefs.SetInt("height", 1200);
+            }
+            else if (resoldrop.value == 4)
+            {
+                Screen.SetResolution(2560, 1440, fullscreenmode);
+                PlayerPrefs.SetInt("width", 2560);
+                PlayerPrefs.SetInt("height", 1440);
+            }
+            else if (resoldrop.value == 5)
+            {
+                Screen.SetResolution(2560, 1600, fullscreenmode);
+                PlayerPrefs.SetInt("width", 2560);
+                PlayerPrefs.SetInt("height", 1600);
+            }
         }
-        else if (resoldrop.value == 2)
-        {
-            Screen.SetResolution(1920,1080, true);
-        }
-        else if (resoldrop.value == 3)
-        {
-            Screen.SetResolution(1920, 1200, true);
-        }
-        else if (resoldrop.value == 4)
-        {
-            Screen.SetResolution(2560, 1440, true);
-        }
-        else if (resoldrop.value == 5)
-        {
-            Screen.SetResolution(2560, 1600, true);
-        }
+
 
     }
 }
